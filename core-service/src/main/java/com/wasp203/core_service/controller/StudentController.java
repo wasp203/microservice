3cfffdc0f26eeb23e294d2116bc8376ace00f864
wasp203 <99825085+wasp203@users.noreply.github.com>
2025-02-18 00:41:13 +0400

    fix create and update student logic

--- core-service/src/main/java/com/wasp203/core_service/controller/StudentController.java
+++ core-service/src/main/java/com/wasp203/core_service/controller/StudentController.java
@@ -1,42 +1,42 @@
 package com.wasp203.core_service.controller;
 
+import com.wasp203.core_service.dto.CreateStudentRequest;
+import com.wasp203.core_service.dto.UpdateStudentRequest;
 import com.wasp203.core_service.entity.Student;
 import com.wasp203.core_service.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
+@RequiredArgsConstructor
 public class StudentController {
     private final StudentService studentService;
 
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
-
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
     @GetMapping("/{id}")
-    public Student getStudent(@PathVariable Long id){
+    public Student getStudent(@PathVariable Long id) {
         return studentService.getStudent(id);
     }
 
     @PostMapping
-    public Student addStudent(@RequestBody Student student){
-        return studentService.addStudent(student);
+    public Student addStudent(@RequestBody CreateStudentRequest request) {
+        return studentService.addStudent(request);
     }
 
     @PutMapping
-    public Student updateStudent(@RequestBody Student student){
-        return studentService.updateStudent(student);
+    public Student updateStudent(@RequestBody UpdateStudentRequest request) {
+        return studentService.updateStudent(request);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteStudent(@PathVariable Long id){
+    public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
     }
 }
--- core-service/src/main/java/com/wasp203/core_service/service/StudentService.java
+++ core-service/src/main/java/com/wasp203/core_service/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.wasp203.core_service.service;
 
+import com.wasp203.core_service.dto.CreateStudentRequest;
+import com.wasp203.core_service.dto.UpdateStudentRequest;
 import com.wasp203.core_service.entity.Student;
 import com.wasp203.core_service.exception.NotFoundException;
+import com.wasp203.core_service.mapper.StudentMapper;
 import com.wasp203.core_service.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,28 +16,29 @@ import java.util.List;
 public class StudentService {
     private final StudentRepository studentRepository;
 
-    public List<Student> getStudents(){
+    private final StudentMapper studentMapper;
+
+    public List<Student> getStudents() {
         return studentRepository.findAll();
     }
 
-    public Student getStudent(Long id){
+    public Student getStudent(Long id) {
         return studentRepository.findById(id).orElseThrow(
                 () -> new NotFoundException("Student with id " + id + " not found"));
     }
 
-    public Student addStudent(Student student){
-        return studentRepository.save(student);
+    public Student addStudent(CreateStudentRequest request) {
+        return studentRepository.save(studentMapper.toEntity(request));
     }
 
-    public Student updateStudent(Student student){
-        if(!studentRepository.existsById(student.getId()))
-            throw new NotFoundException("Student with id " + student.getId() + " not found");
-
-        return studentRepository.save(student);
+    public Student updateStudent(UpdateStudentRequest request) {
+        Student student = studentRepository.findById(request.id()).orElseThrow(
+                () -> new NotFoundException("Student with id " + request.id() + " not found"));
+        return studentRepository.save(studentMapper.partialUpdate(request, student));
     }
 
-    public void deleteStudent(Long id){
-        if(!studentRepository.existsById(id))
+    public void deleteStudent(Long id) {
+        if (!studentRepository.existsById(id))
             throw new NotFoundException("Student with id " + id + " not found");
 
         studentRepository.deleteById(id);
